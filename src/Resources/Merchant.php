fdd1d46963723a252422065e93f28957fd82d4d8
Gus de Boer <gus.de.boer@freshheads.com>
2023-04-03 10:40:41 +0200

    test setters

--- src/Resources/Merchant.php
+++ src/Resources/Merchant.php
@@ -2,14 +2,36 @@
 
 namespace Gusdeboer\OPP\Resources;
 
+use Gusdeboer\OPP\Types\MerchantType;
+
 final readonly class Merchant implements ResourceInterface
 {
+
+    private string $uid;
+    private string $country;
+    private string $emailAddress;
+    private MerchantType $type;
+
     public function __construct()
     {
     }
 
-    public function getUid(): string
+
+    public function setUid(string $uid): self
     {
-        return $this->uid;
+        $this->uid = $uid;
+        return $this;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function setEmailAddress(string $emailAddress): self
+    {
+        $this->emailAddress = $emailAddress;
+        return $this;
     }
 }
