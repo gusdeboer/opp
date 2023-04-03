201544f2f769e683edb1da048ec11e3838f8ee58
Gus de Boer <gus.de.boer@freshheads.com>
2023-04-03 11:25:55 +0200

    remove example json

--- src/Endpoints/MerchantEndpoint.php
+++ src/Endpoints/MerchantEndpoint.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Gusdeboer\OPP\Endpoints;
 
 use Assert\Assertion;
+use Gusdeboer\OPP\Exceptions\ExceptionFactory;
 use Gusdeboer\OPP\Resources\Merchant;
 use Gusdeboer\OPP\Resources\ResourceInterface;
 use Gusdeboer\OPP\Resources\ResourceListInterface;
@@ -24,6 +25,8 @@ readonly class MerchantEndpoint extends AbstractEndpoint implements CrudEndpoint
                 $requestBody
             ]
         ]);
+
+        return $this->serializer->deserialize($request->getBody()->getContents(), Merchant::class, 'json');
     }
 
     public function retrieve(string $uid): Merchant
@@ -31,38 +34,10 @@ readonly class MerchantEndpoint extends AbstractEndpoint implements CrudEndpoint
         $request = $this->client->request('GET', self::ENDPOINT);
 
         if ($request->getStatusCode() !== 200) {
+            //ExceptionFactory::createFromRequest();
         }
 
-        $json = '{
-    "uid": "adkghaognkgksnfg",
-    "object": "merchant",
-    "created": 1554113700,
-    "updated": 1554113700,
-    "status": "pending",
-    "compliance": {
-        "level": 100,
-        "status": "verified",
-        "overview_url": "https://sandbox.onlinebetaalplatform.nl/nl/{{slug}}/{{merchant_uid}}/{{hash}}/overzicht",
-        "requirements": []
-    },
-    "type": "consumer",
-    "coc_nr": null,
-    "name": "John Tester",
-    "phone": "31601234567",
-    "vat_nr": null,
-    "country": "nld",
-    "sector": null,
-    "addresses": [],
-    "trading_names": [],
-    "contacts": [],
-    "profiles": [],
-    "payment_methods": [],
-    "notify_url": "https://platform.com/notify",
-    "return_url": "https://platform.com/return",
-    "metadata": []
-}';
-
-        return $this->serializer->deserialize($json, Merchant::class, 'json');
+        return $this->serializer->deserialize($request->getBody()->getContents(), Merchant::class, 'json');
     }
 
     public function list(int $page = 1, int $limit = 20, array $options = []): ResourceListInterface
